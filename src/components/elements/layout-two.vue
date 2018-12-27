2e3c43cff1c9c3cd246108e8fe9b9a0b4f5225fb
王小龙 <1036199213@qq.com>
2018-12-27 17:35:04 +0800

    layout

--- src/components/elements/layout-two.vue
+++ src/components/elements/layout-two.vue
@@ -1,35 +1,35 @@
 <template>
     <el-row :gutter="20">
-        <el-col :span="8">
+        <el-col :span="8" v-for="n in column" :key="n">
             <div class="grid-content bg-purple">
-                <gui-dragable-box ref="c1"></gui-dragable-box>
+                <gui-dragable-box ref="column"></gui-dragable-box>
             </div>
-        </el-col>
-        <el-col :span="8">
-            <div class="grid-content bg-purple">
-                <gui-dragable-box ref="c2"></gui-dragable-box>
-            </div>
-        </el-col>
-        <el-col :span="8">
-            <div class="grid-content bg-purple">
-               <gui-dragable-box ref="c3"></gui-dragable-box>
-            </div>
-        </el-col>
+        </el-col> 
     </el-row>
 </template>
 <script>
 import element from "../element";
 export default {
     extends:element,
+    props:{
+        column:{
+            type:Number,
+            default:1
+        }
+    },
     data(){
         return{
 
         }
     },
     mounted(){
-        this.childs.push(this.$refs.c1);
-        this.childs.push(this.$refs.c1);
-        this.childs.push(this.$refs.c1);
+        console.log(this.$refs.column);
+        this.$refs.column.map((item)=>{
+            this.childs.push(item);
+        });
+        // this.childs.push(this.$refs.c1);
+        // this.childs.push(this.$refs.c1);
+        // this.childs.push(this.$refs.c1);
     }
 }
 </script>
